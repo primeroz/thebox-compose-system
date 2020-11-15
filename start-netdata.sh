02659a560ae2b5907c585375ef10e3b6db54da76
Francesco Ciocchetti <fciocchetti@mintel.com>
2020-11-15 18:44:52 +0100

    Allow netdata to collect name of docker container

--- start-netdata.sh
+++ start-netdata.sh
@@ -2,8 +2,10 @@
 
 docker run -d --name=netdata \
   -p 19999:19999 \
+  -e PGID=111 \
   -v /proc:/host/proc:ro \
   -v /sys:/host/sys:ro \
+  -v /srv/netdata:/var/cache/netdata \
   -v /var/run/docker.sock:/var/run/docker.sock:ro \
   --cap-add SYS_PTRACE \
   --security-opt apparmor=unconfined \
